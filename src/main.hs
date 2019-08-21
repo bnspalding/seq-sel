98b2ff5eb00cf043d4e40d2adce8557b8ff9b97e
Ben Spalding <bnspalding418@gmail.com>
2019-08-21 10:52:06 -0400

    move version and program options out of main

--- src/main.hs
+++ src/main.hs
@@ -22,35 +22,39 @@ main = do
                 (helper <*> versionOption <*> programOptions)
                 (fullDesc <> progDesc "Sequence, Selection" <>
                 header "Sequence, Selection - program description")
-        versionOption :: Parser (a -> a)
-        versionOption = infoOption "0.0" (long "version" <> help "Show version")
-        programOptions :: Parser Opts
-        programOptions = Opts 
-            <$> strOption 
-                 ( long "func"
-                <> short 'f'  
-                <> metavar "FUNC" 
-                <> help "specify the sequence function")
-            <*> option auto
-                 ( long "lines" 
-                <> short 'l' 
-                <> metavar "LINES" 
-                <> value 4 
-                <> help "specify the number of lines to generate") 
-            <*> strOption
-                 ( long "rhyme" 
-                <> short 'r' 
-                <> metavar "SCHEME" 
-                <> value "abab" 
-                <> help "specify the rhyme scheme")
-            <*> strOption
-                 ( long "meter" 
-                <> short 'm' 
-                <> metavar "SCHEME" 
-                <> value "01010101/010101" 
-                <> help "specify the meter")
-            <*> strOption
-                 ( long "config" 
-                <> metavar "FILENAME.yaml"
-                <> value "none" 
-                <> help "specify options as a .yaml config file")
+
+
+versionOption :: Parser (a -> a)
+versionOption = infoOption "0.0" (long "version" <> help "Show version")
+
+programOptions :: Parser Opts
+programOptions = Opts 
+    <$> strOption 
+            ( long "func"
+        <> short 'f'  
+        <> metavar "FUNC" 
+        <> help "specify the sequence function")
+    <*> option auto
+            ( long "lines" 
+        <> short 'l' 
+        <> metavar "LINES" 
+        <> value 4 
+        <> help "specify the number of lines to generate") 
+    <*> strOption
+            ( long "rhyme" 
+        <> short 'r' 
+        <> metavar "SCHEME" 
+        <> value "abab" 
+        <> help "specify the rhyme scheme")
+    <*> strOption
+            ( long "meter" 
+        <> short 'm' 
+        <> metavar "SCHEME" 
+        <> value "01010101/010101" 
+        <> help "specify the meter")
+    <*> strOption
+            ( long "config"
+        <> short 'c' 
+        <> metavar "FILENAME.yaml"
+        <> value "none" 
+        <> help "specify options as a .yaml config file")
