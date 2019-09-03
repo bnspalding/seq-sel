b68248c49a43598e03d22a97e7748d85ed110552
Ben Spalding <bnspalding418@gmail.com>
2019-09-03 17:08:19 -0400

    Word tests for sound and stress

--- Sound/test/Sound/WordSpec.hs
+++ Sound/test/Sound/WordSpec.hs
@@ -2,7 +2,33 @@ module Sound.WordSpec
   ( spec
   ) where
 
+import Sound.Sound
+import Sound.Stress
+import qualified Sound.Syl as Syl
+import qualified Sound.Word as W
 import Test.Hspec
 
 spec :: Spec
-spec = describe "Sound.Word" $ do it "no tests written" $ do pending
+spec =
+  describe "Sound.Word" $ do
+    it "stress 'pʌmp.kɪn' -> [Stressed, Unstressed] " $
+      W.stress demoWord `shouldBe` [Stressed, Unstressed]
+    it "sounds 'pʌmp.kɪn' -> [p, ʌ, m, p, k, ɪ, n] " $
+      W.sounds demoWord `shouldBe`
+      (Sound <$> ["p", "ʌ", "m", "p", "k", "ɪ", "n"])
+
+demoWord :: W.Word
+demoWord =
+  [ Syl.Syl
+      { Syl.onset = Sound <$> ["p"]
+      , Syl.nucleus = Sound <$> ["ʌ"]
+      , Syl.coda = Sound <$> ["m", "p"]
+      , Syl.stress = Stressed
+      }
+  , Syl.Syl
+      { Syl.onset = Sound <$> ["k"]
+      , Syl.nucleus = Sound <$> ["ɪ"]
+      , Syl.coda = Sound <$> ["n"]
+      , Syl.stress = Unstressed
+      }
+  ]
