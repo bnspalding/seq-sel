e79c71389edab8c80c770c544783cd2a28cc811b
Ben Spalding <bnspalding418@gmail.com>
2019-09-03 16:00:36 -0400

    data correctness tests for GenAm

--- Sound/test/Sound/GenAmSpec.hs
+++ Sound/test/Sound/GenAmSpec.hs
@@ -2,7 +2,78 @@ module Sound.GenAmSpec
   ( spec
   ) where
 
+import qualified Data.Set as Set
+import qualified Sound.GenAm as GenAm
+import Sound.Sound
 import Test.Hspec
 
 spec :: Spec
-spec = describe "Sound.GenAm" $ do it "no tests written" $ do pending
+spec =
+  describe "Sound.GenAm" $ do
+    describe "data correctness" $ do
+      it "consonants are: m n ŋ p b t d k ɡ t͡ʃ d͡ʒ f v θ ð s z ʃ ʒ h l ɹ j ʍ w" $
+        GenAm.sounds `shouldSatisfy` (Set.isProperSubsetOf consonants)
+      it "vowels are: i ɪ ɛ æ ə ʌ ɑ u ʊ ɔ e͡ɪ a͡ɪ a͡ʊ o͡ʊ ɔ͡ɪ ɜ˞ ə˞" $
+        GenAm.sounds `shouldSatisfy` (Set.isProperSubsetOf vowels)
+      it "does not contain any other symbols than those given above" $
+        GenAm.sounds `shouldSatisfy` (== (Set.union vowels consonants))
+      it "does not contain common mistypes: e a o r g ɑ͡ɪ ɛ˞ y" $
+        GenAm.sounds `shouldSatisfy` (Set.disjoint mistypes)
+      it "contains no duplicate sounds (by feature set)" $ pending
+
+consonants :: Set.Set Sound
+consonants =
+  Set.fromList
+    (Sound <$>
+     [ "m"
+     , "n"
+     , "ŋ"
+     , "p"
+     , "b"
+     , "t"
+     , "d"
+     , "k"
+     , "ɡ"
+     , "t͡ʃ"
+     , "d͡ʒ"
+     , "f"
+     , "v"
+     , "θ"
+     , "ð"
+     , "s"
+     , "z"
+     , "ʃ"
+     , "ʒ"
+     , "h"
+     , "l"
+     , "ɹ"
+     , "j"
+     , "ʍ"
+     , "w"
+     ])
+
+vowels :: Set.Set Sound
+vowels =
+  Set.fromList
+    (Sound <$>
+     [ "i"
+     , "ɪ"
+     , "ɛ"
+     , "æ"
+     , "ə"
+     , "ʌ"
+     , "ɑ"
+     , "u"
+     , "ʊ"
+     , "ɔ"
+     , "e͡ɪ"
+     , "a͡ɪ"
+     , "a͡ʊ"
+     , "o͡ʊ"
+     , "ɔ͡ɪ"
+     , "ɜ˞"
+     , "ə˞"
+     ])
+
+mistypes :: Set.Set Sound
+mistypes = Set.fromList (Sound <$> ["e", "a", "o", "r", "g", "ɑ͡ɪ", "ɛ˞", "y"])
