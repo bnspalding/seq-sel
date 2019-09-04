78a58f79b8e144d3eba8651d44054945d36d9c00
Ben Spalding <bnspalding418@gmail.com>
2019-09-04 14:37:58 -0400

    Rhyme/Strict first pass

--- Sound/src/Rhyme/Strict.hs
+++ Sound/src/Rhyme/Strict.hs
@@ -3,10 +3,10 @@ module Rhyme.Strict where
 import qualified Sound.Syl as Syl
 
 rhyme :: Syl.Syl -> Syl.Syl -> Bool
-rhyme = undefined
+rhyme syl1 syl2 = Syl.rhyme syl1 == Syl.rhyme syl2
 
 assonance :: Syl.Syl -> Syl.Syl -> Bool
-assonance = undefined
+assonance syl1 syl2 = Syl.nucleus syl1 == Syl.nucleus syl2
 
 alliteration :: Syl.Syl -> Syl.Syl -> Bool
-alliteration = undefined
+alliteration syl1 syl2 = Syl.onset syl1 == Syl.onset syl2
