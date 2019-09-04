4035214a0dcd461df378221757a9e3760d33beff
Ben Spalding <bnspalding418@gmail.com>
2019-09-04 13:59:48 -0400

    first pass on Rhyme.Approx

--- Sound/src/Rhyme/Approx.hs
+++ Sound/src/Rhyme/Approx.hs
@@ -4,13 +4,31 @@ module Rhyme.Approx
   , alliteration
   ) where
 
+import qualified Data.Set as Set
+import Sound.Feature
+import qualified Sound.GenAm as GenAm
+import Sound.Sound
 import qualified Sound.Syl as Syl
 
-rhyme :: Syl.Syl -> Syl.Syl -> Rational
-rhyme = undefined
+rhyme :: Syl.Syl -> Syl.Syl -> Float
+rhyme syl1 syl2 = _compare Syl.rhyme syl1 syl2
 
-assonance :: Syl.Syl -> Syl.Syl -> Rational
-assonance = undefined
+assonance :: Syl.Syl -> Syl.Syl -> Float
+assonance syl1 syl2 = _compare Syl.nucleus syl1 syl2
 
-alliteration :: Syl.Syl -> Syl.Syl -> Rational
-alliteration = undefined
+alliteration :: Syl.Syl -> Syl.Syl -> Float
+alliteration syl1 syl2 = _compare Syl.onset syl1 syl2
+
+_compare :: (Syl.Syl -> [Sound]) -> Syl.Syl -> Syl.Syl -> Float
+_compare f syl1 syl2 =
+  (fromIntegral (Set.size $ Set.intersection fs1 fs2)) /
+  (fromIntegral (Set.size $ Set.union fs1 fs2))
+  where
+    fs1 = _merge $ _featuresOf $ f syl1
+    fs2 = _merge $ _featuresOf $ f syl2
+
+_featuresOf :: [Sound] -> [FeatureSet]
+_featuresOf ss = ((\(Just x) -> x) . GenAm.features) <$> ss
+
+_merge :: [FeatureSet] -> FeatureSet
+_merge fsS = foldl (Set.union) Set.empty fsS
