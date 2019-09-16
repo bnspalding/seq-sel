e53f9195c7dd48576ef881f884e9f65587afe686
Ben Spalding <bnspalding418@gmail.com>
2019-09-16 15:41:40 -0400

    begin work on dictionary structure

---
+++ Dictionary/src/Dictionary.hs
@@ -0,0 +1,38 @@
+module Dictionary where
+
+import qualified Data.Set as Set
+
+type Dictionary = Set.Set String
+
+fromList :: [String] -> Dictionary
+fromList = Set.fromList
+
+first :: Dictionary -> String
+first = Set.elemAt 0
+
+last :: Dictionary -> String
+last d = Set.elemAt (size d - 1) d
+
+size :: Dictionary -> Int
+size = Set.size
+
+next :: Dictionary -> String -> String
+next d entry = Set.elemAt i d
+  where
+    prevI = Set.findIndex entry d
+    i =
+      if prevI == size d - 1
+        then 0
+        else prevI + 1
+
+prev :: Dictionary -> String -> String
+prev d entry = Set.elemAt i d
+  where
+    nextI = Set.findIndex entry d
+    i =
+      if nextI == 0
+        then size d - 1
+        else nextI - 1
+
+contains :: Dictionary -> String -> Bool
+contains d entry = Set.member entry d
