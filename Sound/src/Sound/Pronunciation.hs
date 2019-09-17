09b77eaafbb72a466a1e3956dbf06562af5b08bd
Ben Spalding <bnspalding418@gmail.com>
2019-09-17 11:56:25 -0400

    more Pronunciation work

--- Sound/src/Sound/Pronunciation.hs
+++ Sound/src/Sound/Pronunciation.hs
@@ -5,9 +5,20 @@ import Sound
 import Sound.IPA
 import Sound.Syllabify
 
-type PronunciationTable = Map.Map String Sound.Word
+type PronunciationTable = Map.Map String Pronunciation
+
+type Pronunciation = Sound.Word
 
 fromList :: [(String, String)] -> PronunciationTable
 fromList mappings = Map.fromList $ valToSounds <$> mappings
   where
     valToSounds (k, v) = (k, syllabify $ stringToIPASounds v)
+
+stripStress :: Pronunciation -> Pronunciation
+stripStress = undefined
+
+readStress :: Pronunciation -> [Stress]
+readStress = undefined -- march over vowels and adjacent stress marks (if present)
+
+lookup :: PronunciationTable -> String -> Maybe Pronunciation
+lookup t s = Map.lookup s t
