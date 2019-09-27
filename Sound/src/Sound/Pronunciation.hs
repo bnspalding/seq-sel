a5d5253ad2de47f93af391f1df29801b754dcbfc
Ben Spalding <bnspalding418@gmail.com>
2019-09-27 10:31:47 -0400

    cleaning up Sound/Pronunciation

--- Sound/src/Sound/Pronunciation.hs
+++ Sound/src/Sound/Pronunciation.hs
@@ -12,16 +12,7 @@ type Pronunciation = Sound.Word
 fromList :: [(String, String)] -> PronunciationTable
 fromList mappings = Map.fromList $ valToSounds <$> mappings
   where
-    valToSounds (k, v) = (k, syllabify $ stringToIPASounds v)
+    valToSounds (word, ipa) = (word, syllabify $ stringToIPASounds ipa)
 
--- stripStress and readStress are currently rolled into syllabify
--- at some point in the future it may make sense to pull them out
--- but for now it's not necessary
--- Sound.Word already has sounds() and stress() to access this information
--- stripStress :: Pronunciation -> Pronunciation
--- stripStress = 
--- 
--- readStress :: Pronunciation -> [Stress]
--- readStress = -- march over vowels and adjacent stress marks (if present)
 lookup :: PronunciationTable -> String -> Maybe Pronunciation
 lookup t s = Map.lookup s t
