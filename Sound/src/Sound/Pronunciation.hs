38019c96642b228ed386a26ac0562b2a7ffecc18
Ben Spalding <bnspalding418@gmail.com>
2019-09-27 13:15:24 -0400

    add note to Pronunciations about collapsing pronunciations (ɹid/ɹɛd)

--- Sound/src/Sound/Pronunciation.hs
+++ Sound/src/Sound/Pronunciation.hs
@@ -5,6 +5,8 @@ import Sound
 import Sound.IPA
 import Sound.Syllabify
 
+--NOTE: using a map to a single Pronunciation means that instances like
+-- read/read (ɹid/ɹɛd) are reduced to a single pronunciation
 type PronunciationTable = Map.Map String Pronunciation
 
 type Pronunciation = Sound.Word
