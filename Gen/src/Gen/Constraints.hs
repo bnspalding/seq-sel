b66bd23488f22891614ab0466a6bcb0672816776
Ben Spalding <bnspalding418@gmail.com>
2019-12-04 15:20:09 -0500

    Constraint should be a type alias, not a new type

--- Gen/src/Gen/Constraints.hs
+++ Gen/src/Gen/Constraints.hs
@@ -4,8 +4,7 @@ import qualified Data.Map as Map
 import Sound
 import Sound.Syl
 
-newtype Constraint =
-  Constraint (Syl -> Bool)
+type Constraint = (Syl -> Bool)
 
 makeCons :: Int -> String -> String -> Float -> String -> [[[[Constraint]]]]
 makeCons lineCount meterS rhymeS rhymeThreshold customCons = undefined
