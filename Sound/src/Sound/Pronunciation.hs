854b7e2061e0c76833dbb3cfd25e163a4d5b5f4f
Ben Spalding <bnspalding418@gmail.com>
2019-12-04 14:37:30 -0500

    simplify Pronunciation. Pronunciation tables are merging into Dictionary

--- Sound/src/Sound/Pronunciation.hs
+++ Sound/src/Sound/Pronunciation.hs
@@ -5,16 +5,7 @@ import Sound
 import Sound.IPA
 import Sound.Syllabify
 
---NOTE: using a map to a single Pronunciation means that instances like
--- read/read (ɹid/ɹɛd) are reduced to a single pronunciation
-type PronunciationTable = Map.Map String Pronunciation
-
 type Pronunciation = Sound.Word
 
-fromList :: [(String, String)] -> PronunciationTable
-fromList mappings = Map.fromList $ valToSounds <$> mappings
-  where
-    valToSounds (word, ipa) = (word, syllabify $ stringToIPASounds ipa)
-
-lookup :: PronunciationTable -> String -> Maybe Pronunciation
-lookup t s = Map.lookup s t
+makePronunciation :: String -> Pronunciation
+makePronunciation = syllabify . stringToIPASounds
