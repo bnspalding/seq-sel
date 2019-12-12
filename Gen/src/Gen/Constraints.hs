71238b6961185fcb061008ccd7fc59f0aeb710b4
Ben Spalding <bnspalding418@gmail.com>
2019-12-12 16:27:26 -0500

    constraint work (messy)

--- Gen/src/Gen/Constraints.hs
+++ Gen/src/Gen/Constraints.hs
@@ -38,6 +38,18 @@ type PoemCons = [StanzaCons]
 
 type SylLoc = (Int, Int, Int) -- the [Stanza] [Line] [Syl] address of a Syl
 
+emptyPoem :: PoemCons
+emptyPoem = [emptyStanza]
+
+emptyStanza :: StanzaCons
+emptyStanza = [emptyLine]
+
+emptyLine :: LineCons
+emptyLine = [emptySyl]
+
+emptySyl :: SylCons
+emptySyl = ([], [])
+
 makeCons :: Int -> String -> String -> Float -> String -> PoemCons
 makeCons linesN meterS rhymeS rhymeThreshold customCons = undefined
 
@@ -53,6 +65,9 @@ lineCount = length
 sylCount :: LineCons -> Int
 sylCount = length
 
+totalLineCount :: PoemCons -> Int
+totalLineCount p = sum $ lineCount <$> p
+
 constraintsAt :: PoemCons -> SylLoc -> SylCons
 constraintsAt p (stanzaI, lineI, sylI) = p !! stanzaI !! lineI !! sylI
 
@@ -60,10 +75,85 @@ addRhymeScheme :: PoemCons -> String -> PoemCons
 addRhymeScheme p rhymeS = undefined
 
 addMeterScheme :: PoemCons -> String -> PoemCons
-addMeterScheme = undefined
+addMeterScheme p meterS = mergePoems p (toMeterCons p meterS)
+
+toMeterCons :: PoemCons -> String -> PoemCons
+toMeterCons startingP scheme = toMC_recursive scheme startingP (0, 0, 0)
+  where
+    toMC_recursive [] p _ = p
+    toMC_recursive (c:cs) p (st, ln, sy)
+      | c == '0' =
+        toMC_recursive
+          cs
+          (addConstraintAt p (st, ln, sy) (makeMeterConstraint Unstressed))
+          (st, ln, sy + 1)
+      | c == '1' =
+        toMC_recursive
+          cs
+          (addConstraintAt p (st, ln, sy) (makeMeterConstraint Stressed))
+          (st, ln, sy + 1)
+      | c == '/' = toMC_recursive cs p (st, ln + 1, 0) -- linebreak
+      | c == 's' = toMC_recursive cs p (st + 1, 0, 0) -- stanzabreak
+      | otherwise = error $ "unknown symbol" ++ [c] ++ "in meter cons"
 
 addConstraintAt :: PoemCons -> SylLoc -> (Constraint, SpecMod) -> PoemCons
-addConstraintAt p (stanzaI, lineI, sylI) c = undefined
+addConstraintAt p loc@(stanzaI, lineI, sylI) c
+  | stanzaI >= stanzaCount p = againWithAnotherStanza
+  | lineI >= lineCount (p !! stanzaI) = againWithAnotherLine
+  | sylI >= sylCount (p !! stanzaI !! lineI) = againWithAnotherSyl
+  | otherwise = addConToSyl loc p c
+  where
+    againWithAnotherStanza = addConstraintAt (addEmptyStanza p) loc c
+    againWithAnotherLine = addConstraintAt (addEmptyLine stanzaI p) loc c
+    againWithAnotherSyl = addConstraintAt (addEmptySyl stanzaI lineI p) loc c
+
+addEmptyStanza :: PoemCons -> PoemCons
+addEmptyStanza p = p ++ [emptyStanza]
+
+addEmptyLine :: Int -> PoemCons -> PoemCons
+addEmptyLine stanzaI p = replace stanzaI modifiedStanza p
+  where
+    modifiedStanza = p !! stanzaI ++ [emptyLine]
+
+addEmptySyl :: Int -> Int -> PoemCons -> PoemCons
+addEmptySyl stanzaI lineI p = replace stanzaI modifiedStanza p
+  where
+    modifiedStanza = replace lineI modifiedLine (p !! stanzaI)
+    modifiedLine = p !! stanzaI !! lineI ++ [emptySyl]
+
+addConToSyl :: SylLoc -> PoemCons -> (Constraint, SpecMod) -> PoemCons
+addConToSyl (stanzaI, lineI, sylI) p c = modifiedPoem
+  where
+    modifiedSyl = mergeCons (p !! stanzaI !! lineI !! sylI) c
+    modifiedLine = replace sylI modifiedSyl (p !! stanzaI !! lineI)
+    modifiedStanza = replace lineI modifiedLine (p !! stanzaI)
+    modifiedPoem = replace stanzaI modifiedStanza p
+
+mergeCons :: SylCons -> (Constraint, SpecMod) -> SylCons
+mergeCons (cs, ms) (c, m) = (c : cs, m : ms)
+
+mergePoems :: PoemCons -> PoemCons -> PoemCons
+mergePoems = mergeF mergeStanzas
+
+mergeStanzas :: StanzaCons -> StanzaCons -> StanzaCons
+mergeStanzas = mergeF mergeLines
+
+mergeLines :: LineCons -> LineCons -> LineCons
+mergeLines = mergeF mergeSyls
+
+mergeSyls :: SylCons -> SylCons -> SylCons
+mergeSyls (c1s, m1s) (c2s, m2s) = (c1s ++ c2s, m1s ++ m2s)
+
+mergeF :: (a -> a -> a) -> [a] -> [a] -> [a]
+mergeF _ [] ys = ys
+mergeF _ xs [] = xs
+mergeF f (x:xs) (y:ys) = f x y : mergeF f xs ys
+
+replace :: Int -> a -> [a] -> [a]
+replace i x xs =
+  case splitAt i xs of
+    (before, []) -> before ++ [x]
+    (before, _:after) -> before ++ [x] ++ after
 
 makeRhymeConstraint :: Char -> Float -> (Constraint, SpecMod)
 makeRhymeConstraint c rThreshold =
@@ -81,7 +171,10 @@ makeRhymeConstraint c rThreshold =
    in (con, upd)
 
 makeMeterConstraint :: Stress -> (Constraint, SpecMod)
-makeMeterConstraint s = undefined
+makeMeterConstraint s =
+  let con syl spec = stress syl == s --does syl stress == specified stress
+      upd spec _ = spec -- no change to spec
+   in (con, upd)
 
 selectRhymeFunc :: Float -> (Syl -> Syl -> Bool)
 selectRhymeFunc rThreshold
