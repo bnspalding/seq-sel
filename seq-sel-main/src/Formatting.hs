cb6f3f655985952bf5a826cb21611774e97cbf84
Ben Spalding <bnspalding418@gmail.com>
2020-05-05 11:13:44 -0400

    write outline for Formatting interface

---
+++ seq-sel-main/src/Formatting.hs
@@ -0,0 +1,9 @@
+module Formatting where
+
+-- TODO: move code for formatting a generated poem to its written form here
+--
+-- writePoem :: Poem -> T.Text
+-- writeAnnotation :: Annotation -> T.Text (dispatch on type)
+-- appendAnnotations :: T.Text -> [Annotation] -> T.Text
+-- appendAnnotation :: T.Text -> Annotation -> T.Text
+-- writeToFile :: T.Text -> IO ()
